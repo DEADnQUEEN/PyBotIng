e513ba4dff2694614948e9f0e4c7cd56329e805b
DeadQueen <aleks20aleks0228@gmail.com>
2024-01-13 14:18:01 +0300

    Small changes

--- sub_functions.py
+++ sub_functions.py
@@ -14,6 +14,10 @@ from telebot import types
 
 
 def path_exists(path: str) -> None:
+
+    for sub_path in list(path.split('\\')):
+        path_exists(sub_path)
+
     if not os.path.exists(path):
         print(f'dir "{path}" have been added')
         os.mkdir(path)
